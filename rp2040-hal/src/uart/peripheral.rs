5b87bbc1ec290341182d1f0620cb2cdf57deed1b
DmitriLyalikov <dlyalikov01@manhattan.edu>
2023-06-06 12:22:32 -0400

    Update peripheral.rs

--- rp2040-hal/src/uart/peripheral.rs
+++ rp2040-hal/src/uart/peripheral.rs
@@ -139,9 +139,10 @@ impl<D: UartDevice, P: ValidUartPinout<D>> UartPeripheral<Enabled, D, P> {
     }
 
     pub fn clear_rx_interrupt(&mut self) {
-        self.device.uarticr.write(|w| unsafe {
-            w.rxic().set_bit();
-        });
+        self.device.uarticr.write
+        (|w| unsafe {
+            w.rxic().set_bit()
+        })
     }
 
     /// Enables the Transmit Interrupt.
